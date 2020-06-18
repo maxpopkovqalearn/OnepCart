4861e342a58dcc34ca7c9c364b31a44331d47eb8
Max P <maxpopkovvv123@gmail.com>
2020-06-18 09:40:55 +0300

    UP ver 2. Переделка с 0

--- tests/tests_for_open_cart.py
+++ tests/tests_for_open_cart.py
@@ -1,17 +1,18 @@
-
+from selenium.webdriver.common.by import By
 
 def test_element_main_page(browser):
     """Tests for main page"""
     browser.find_element_by_class_name('navbar-header')
-    browser.find_element_by_xpath()
-    browser.find_element_by_id()
-    browser.find_element_by_css_selector()
-    browser.find_element_by_name()
+    browser.find_element_by_xpath('//*[text()="Wish List"]')
+    browser.find_element_by_link_text('Shopping Cart')
+    browser.find_element_by_css_selector('a[href="http://localhost/Opencart/index.php?route=product/category&path=57"]')
+    browser.find_element_by_name('search')
+
 
 def test_element_catalog_page(browser):
     """Tests for catalog page"""
     pass
-    # browser.find_element_by_class_name('navbar-header')
+    # browser.find_element_by_class_name('')
     # browser.find_element_by_xpath()
     # browser.find_element_by_id()
     # browser.find_element_by_css_selector()
